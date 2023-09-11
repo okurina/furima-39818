6de27122fa7f44b32a2531462e36efcb7856cbcc
okurina <rio.60821023@gmail.com>
2023-09-11 16:54:04 +0900

    価格のバリテーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,8 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipment_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+             format: { with: /\A[0-9]+\z/ }
   validates :image, presence: true
   
   belongs_to :category
