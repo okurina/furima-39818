cff982afaceacd74a82d685745eb0eed920fa06b
okurina <rio.60821023@gmail.com>
2023-09-11 16:03:20 +0900

    バリテーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,19 @@
 class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-
   belongs_to :category
   belongs_to :situation
   belongs_to :delivery_charge
   belongs_to :region
   belongs_to :shipment
-  
+
+  validates :title, :text, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipment_id, numericality: { other_than: 1 , message: "can't be blank"}
+
   belongs_to :user
 
 end
