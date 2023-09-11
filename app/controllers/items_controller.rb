1a60508b3a8a264476658d6cd68ae2a8387fba2b
okurina <rio.60821023@gmail.com>
2023-09-11 22:04:11 +0900

    ビューファイルの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.all
   end
+
+  def new
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+  end 
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id, :situation_id, :delivery_charge_id, :region_id, :shipment_id, :price, :item_name, :content, :image).merge(user_id: current_user.id)
+  end
+
+
 end
