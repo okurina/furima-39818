38e6d7d6646ecd4e98540bceb169c8e38f2db127
okurina <rio.60821023@gmail.com>
2023-09-11 16:38:58 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,21 @@
 class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :situation
-  belongs_to :delivery_charge
-  belongs_to :region
-  belongs_to :shipment
-
   validates :item_name, :content, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipment_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :image, presence: true
+  
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery_charge
+  belongs_to :region
+  belongs_to :shipment
 
   belongs_to :user
+  has_one_attached :image
 
 end
