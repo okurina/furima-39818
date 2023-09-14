db084957c25d18b71448487e3b91d5ab58afcf76
okurina <rio.60821023@gmail.com>
2023-09-14 16:49:57 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
 
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    item = Item.includes(:user).find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
