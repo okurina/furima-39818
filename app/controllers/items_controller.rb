0d3278437dd1f4692310724cd94a47a2cb011750
okurina <rio.60821023@gmail.com>
2023-09-14 15:47:48 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destoroy]
+  before_action :move_to_index, only: :edit
+ 
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -24,10 +27,31 @@ class ItemsController < ApplicationController
     @item = Item.includes(:user).find(params[:id])
   end
 
+  def edit
+    @item = Item.includes(:user).find(params[:id])
+  end
+
+  def update
+    @item = Item.includes(:user).find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:category_id, :situation_id, :delivery_charge_id, :region_id, :shipment_id, :price, :item_name,
                                  :content, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.includes(:user).find(params[:id])
+    unless user_signed_in? && current_user == @item.user
+      redirect_to action: :index
+    end
+  end
+
 end
