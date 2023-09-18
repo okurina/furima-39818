c49060ea9789e4e5915698be643c1c61f7da1358
okurina <rio.60821023@gmail.com>
2023-09-18 09:04:55 +0900

    エラー文の表示、修正

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,12 +1,16 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+
   def index
     @purchase_record_address = PurchaseRecordAddress.new
-    @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
-  def create
-    binding.pry
-    @purchase_record_address = PurchaseRecordAddress.new(purchase_record_address_params)
+  def create 
+    @purchase_record_address = PurchaseRecordAddress.new(purchase_record_params)
     if @purchase_record_address.valid?
       @purchase_record_address.save
       redirect_to root_path
@@ -17,8 +21,12 @@ class PurchaseRecordsController < ApplicationController
 
   private
 
-  def purchase_record_address_params
-    params.require(:purchase_record_address).permit(:post_code, :region_id, :city, :street, :building, :telephone).merge(item_id: params, user_id: current_user.id)
+  def purchase_record_params
+    params.require(:purchase_record_address).permit(:post_cord, :region_id, :city, :street, :building, :telephone).merge(item_id: @item.id, user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -1,18 +1,19 @@
 class PurchaseRecordAddress
   include ActiveModel::Model
-  attr_accessor :item_id,:user_id, :region_id, :post_cord, :city, :street, :building, :telephone
+  attr_accessor :item_id, :user_id, :region_id, :post_cord, :city, :street, :building, :telephone, :purchase_record_id
+
 
   with_options presence: true do
-    validates :item_id, :user_id
+    validates :item_id, :user_id, :purchase_record_id
     validates :city, :street
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_cord, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :telephone, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
   end
   
   validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
   
-  def save
-    purchase_record = PurchaseRecord.create(item_id: params, user_id: user_id)
-    Address.create(post_code: post_code, region_id: region_id, city: city, street: street, building: building, telephone: telephone, purchase_record_id: purchase_record.id)
+  def save(item_id, user_id)
+    purchase_record = PurchaseRecord.create(item_id: item_id, user_id: user_id)
+    Address.create(post_cord: post_cord, region_id: region_id, city: city, street: street, building: building, telephone: telephone, purchase_record_id: purchase_record.id)
   end
 end
